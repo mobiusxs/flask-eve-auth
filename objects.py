de2ad9c99c9b9fc207496191589b17866fc2877a
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-07-15 16:26:31 -0500

    show examples of objects that may be needed

---
+++ objects.py
@@ -0,0 +1,71 @@
+payload = {
+    'scp': ['esi-calendar.respond_calendar_events.v1', 'esi-calendar.read_calendar_events.v1', 'esi-location.read_location.v1', 'esi-location.read_ship_type.v1', 'esi-mail.organize_mail.v1', 'esi-mail.read_mail.v1', 'esi-mail.send_mail.v1', 'esi-skills.read_skills.v1', 'esi-skills.read_skillqueue.v1', 'esi-wallet.read_character_wallet.v1', 'esi-wallet.read_corporation_wallet.v1', 'esi-search.search_structures.v1', 'esi-clones.read_clones.v1', 'esi-characters.read_contacts.v1', 'esi-universe.read_structures.v1', 'esi-bookmarks.read_character_bookmarks.v1', 'esi-killmails.read_killmails.v1', 'esi-corporations.read_corporation_membership.v1', 'esi-assets.read_assets.v1', 'esi-planets.manage_planets.v1', 'esi-fleets.read_fleet.v1', 'esi-fleets.write_fleet.v1', 'esi-ui.open_window.v1', 'esi-ui.write_waypoint.v1', 'esi-characters.write_contacts.v1', 'esi-fittings.read_fittings.v1', 'esi-fittings.write_fittings.v1', 'esi-markets.structure_markets.v1', 'esi-corporations.read_structures.v1', 'esi-characters.read_loyalty.v1', 'esi-characters.read_opportunities.v1', 'esi-characters.read_chat_channels.v1', 'esi-characters.read_medals.v1', 'esi-characters.read_standings.v1', 'esi-characters.read_agents_research.v1', 'esi-industry.read_character_jobs.v1', 'esi-markets.read_character_orders.v1', 'esi-characters.read_blueprints.v1', 'esi-characters.read_corporation_roles.v1', 'esi-location.read_online.v1', 'esi-contracts.read_character_contracts.v1', 'esi-clones.read_implants.v1', 'esi-characters.read_fatigue.v1', 'esi-killmails.read_corporation_killmails.v1', 'esi-corporations.track_members.v1', 'esi-wallet.read_corporation_wallets.v1', 'esi-characters.read_notifications.v1', 'esi-corporations.read_divisions.v1', 'esi-corporations.read_contacts.v1', 'esi-assets.read_corporation_assets.v1', 'esi-corporations.read_titles.v1', 'esi-corporations.read_blueprints.v1', 'esi-bookmarks.read_corporation_bookmarks.v1', 'esi-contracts.read_corporation_contracts.v1', 'esi-corporations.read_standings.v1', 'esi-industry.read_corporation_jobs.v1', 'esi-markets.read_corporation_orders.v1', 'esi-corporations.read_container_logs.v1', 'esi-industry.read_character_mining.v1', 'esi-industry.read_corporation_mining.v1', 'esi-planets.read_customs_offices.v1', 'esi-corporations.read_facilities.v1', 'esi-corporations.read_medals.v1', 'esi-characters.read_titles.v1', 'esi-alliances.read_contacts.v1', 'esi-characters.read_fw_stats.v1', 'esi-corporations.read_fw_stats.v1'],
+    'jti': '52f920f0-51a4-4a2f-906e-2075e6bd6a2f',
+    'kid': 'JWT-Signature-Key',
+    'sub': 'CHARACTER:EVE:94268459',
+    'azp': 'd513b498c32048179f0b32c011d03f30',
+    'tenant': 'tranquility',
+    'tier': 'live',
+    'region': 'world',
+    'aud': 'EVE Online',
+    'name': 'Kim Peek',
+    'owner': '7N2R8Mg9N99DTwQntwQouWRrRik=',
+    'exp': 1657909020,
+    'iat': 1657907820,
+    'iss': 'login.eveonline.com'
+}
+
+character = {
+  "alliance_id": 99003581,
+  "birthday": "2014-01-28T02:18:25Z",
+  "bloodline_id": 8,
+  "corporation_id": 98405234,
+  "description": "<font size=\"13\" color=\"#ff999999\"></font><font size=\"36\" color=\"#ff00ffff\"><b>#2</b></font><font size=\"36\" color=\"#ffffffff\"> </font><font size=\"30\" color=\"#ffffff00\"><i>ranked</i></font><font size=\"12\" color=\"#ffffffff\"> </font><font size=\"24\" color=\"#ff00ff00\"><u>trash-tier</u></font><font size=\"12\" color=\"#ffffffff\"> </font><font size=\"18\" color=\"#ffff0000\"><b>pilot</b></font><font size=\"12\" color=\"#ffffffff\"> </font><font size=\"14\" color=\"#ff00ffff\"><i>in</i></font><font size=\"12\" color=\"#ffffffff\"> </font><font size=\"12\" color=\"#ffffff00\"><u>all</u></font><font size=\"12\" color=\"#ffffffff\"> </font><font size=\"10\" color=\"#ff00ff00\"><b>of</b></font><font size=\"12\" color=\"#ffffffff\"> </font><font size=\"8\" color=\"#ffff0000\"><i>Eve</i><br><br><br></font><font size=\"14\" color=\"#ff4c4c4c\"><b>Back in the day...<br> -Random Shitler<br><br><br><br></font><font size=\"24\" color=\"#ff007fff\">USAF Capt. Reid Nishizuka<br></font><font size=\"14\" color=\"#ffffe400\"><loc><a href=\"https://www.rememberingreid.org/\">https://www.rememberingreid.org/</a><br><a href=\"https://swampland.time.com/2013/10/30/the-crash-of-independence-08/\">https://swampland.time.com/2013/10/30/the-crash-of-independence-08</loc>/</a></b></font>",
+  "gender": "male",
+  "name": "Kim Peek",
+  "race_id": 8,
+  "security_status": -0.943116629
+}
+
+portraits = {
+  "px128x128": "https://images.evetech.net/characters/94268459/portrait?tenant=tranquility&size=128",
+  "px256x256": "https://images.evetech.net/characters/94268459/portrait?tenant=tranquility&size=256",
+  "px512x512": "https://images.evetech.net/characters/94268459/portrait?tenant=tranquility&size=512",
+  "px64x64": "https://images.evetech.net/characters/94268459/portrait?tenant=tranquility&size=64"
+}
+
+corporation = {
+  "alliance_id": 99003581,
+  "ceo_id": 411225042,
+  "creator_id": 411225042,
+  "date_founded": "2015-06-28T20:39:16Z",
+  "description": "u'<font size=\"13\" color=\"#ff999999\"></font><font size=\"12\" color=\"#ff999999\">( \\u2022_\\u2022)<br><br>( \\u2022_\\u2022)&gt;\\u2310\\u25a0-\\u25a0<br><br>(\\u2310\\u25a0_\\u25a0)<br><br></font><font size=\"14\" color=\"#ffffff00\">Admin:<br></font><font size=\"12\" color=\"#ffd98d00\"><a href=\"showinfo:1375//95576014\">Aurora Charis</a></font><font size=\"12\" color=\"#ffffff00\"> <br></font><font size=\"12\" color=\"#ffd98d00\"><a href=\"showinfo:1373//411225042\">BJK</a></font><font size=\"12\" color=\"#ffffff00\"> <br></font><font size=\"12\" color=\"#ffd98d00\"><a href=\"showinfo:1379//2114091385\">etin eram</a></font><font size=\"12\" color=\"#ffffff00\"> <br></font><font size=\"12\" color=\"#ffd98d00\"><a href=\"showinfo:1383//1245093273\">GinniK</a></font><font size=\"12\" color=\"#ffffff00\"> <br></font><font size=\"12\" color=\"#ffd98d00\"><a href=\"showinfo:1373//345706954\">Ledeanio</a></font><font size=\"12\" color=\"#ffffff00\"> <br></font><font size=\"12\" color=\"#ffd98d00\"><a href=\"showinfo:1377//92790395\">Miwanow Kregoolan</a></font><font size=\"12\" color=\"#ffffff00\"> <br></font><font size=\"12\" color=\"#ffd98d00\"><a href=\"showinfo:1380//1314083152\">Ybytlan</a></font><font size=\"12\" color=\"#ffffff00\"> <br><br></font><font size=\"14\" color=\"#ffffff00\">Contact Us:<br></font><font size=\"12\" color=\"#ff6868e1\"><a href=\"joinChannel:-71417797//None//None\">xDECOYx</a></font><font size=\"12\" color=\"#ff999999\"> <br><br></font><font size=\"14\" color=\"#ffffff00\">Recruitment:<br></font><font size=\"12\" color=\"#ffffffff\">Selective</font>'",
+  "home_station_id": 60013075,
+  "member_count": 154,
+  "name": "xDECOYx",
+  "shares": 1000,
+  "tax_rate": 0.10000000149011612,
+  "ticker": "-D0-",
+  "url": "http://",
+  "war_eligible": True
+}
+
+corporation_icons = {
+  "px128x128": "https://images.evetech.net/corporations/98405234/logo?tenant=tranquility&size=128",
+  "px256x256": "https://images.evetech.net/corporations/98405234/logo?tenant=tranquility&size=256",
+  "px64x64": "https://images.evetech.net/corporations/98405234/logo?tenant=tranquility&size=64"
+}
+
+alliance = {
+  "creator_corporation_id": 98241771,
+  "creator_id": 379226154,
+  "date_founded": "2013-08-23T05:45:11Z",
+  "executor_corporation_id": 98688253,
+  "name": "Fraternity.",
+  "ticker": "FRT"
+}
+
+alliance_icons = {
+  "px128x128": "https://images.evetech.net/Alliance/99003581_128.png",
+  "px64x64": "https://images.evetech.net/Alliance/99003581_64.png"
+}
